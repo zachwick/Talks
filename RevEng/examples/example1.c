810662a8eca234cb87caadda34493222948003f0
ZachWick <zach@zachwick.com>
2014-04-21 17:11:50 -0400

    Adds examples to v1 of Rev. Eng. talk for AHA
    Signed-off-by: ZachWick <zach@zachwick.com>

---
+++ RevEng/examples/example1.c
@@ -0,0 +1,17 @@
+/**
+ * RevEng Example #1
+ *
+ * This program was written to be an educational example of how to reverse
+ * engineer software.
+ *
+ * Copyright 2014 ZachWick <zach@zachwick.com>
+ * Licensed under the GPLv3 or later
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	printf("Hello World");
+}
